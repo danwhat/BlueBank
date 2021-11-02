980bc68d3baaba863418cc006221b2084de2bda5
Daniel Oliveira <dan.carvalho94@gmail.com>
2021-11-02 16:33:17 -0300

    Interfaces

---
+++ Domain.Core/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Core.Interfaces
+{
+    interface ITransactionRepository
+    {
+        public Transaction Create(Transaction transaction);
+        public List<Transaction> ReadCotainsAcc(Account acc);
+        public List<Transaction> ReadCotainsAcc(int accountNumber);
+        public List<Transaction> ReadByPeriod(DateTime initial, DateTime final, Account acc);
+        public List<Transaction> ReadByPeriod(DateTime initial, DateTime final, int accountNumber);
+    }
+}
