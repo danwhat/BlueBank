12502c4d5ce6dfc009344e27607631985bc62351
Luciano Berchon <berchon@gmail.com>
2021-11-02 19:00:41 -0300

    Implementada a funcionalidade de procurar conta por número da conta

---
+++ Domain/Requests/GetAccountRequest.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Core.DTOs;
+using Domain.Entities;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Requests
+{
+    public class GetAccountRequest
+    {
+        private readonly int _accountNumber;
+        private readonly AccountRepository _accountRepository;
+
+        public GetAccountRequest(int accountNumber)
+        {
+            _accountNumber = accountNumber;
+            _accountRepository = new AccountRepository();
+        }
+
+        public Account Get()
+        {
+            var account = new Infrastructure.Account();
+
+            // var findAccount = _context.Accounts.SingleOrDefault(x => x.Id == account);
+
+            Account result = _accountRepository.Get(_accountNumber);
+            // int response = new();
+
+            return result;
+        }
+    }
+}
