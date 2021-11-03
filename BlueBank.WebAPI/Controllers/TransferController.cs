3efc8252596f2ef04d9a442f9a3a4f20dfa0fe2b
Daniel Oliveira <dan.carvalho94@gmail.com>
2021-11-03 14:52:26 -0300

    Funcionalidade Transferencia Implementada

---
+++ BlueBank.WebAPI/Controllers/TransferController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Core.DTOs;
+using Domain.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlueBank.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferController : ControllerBase
+    {
+        [HttpPost("{accountNumber}")]
+        public ObjectResult Transfer(int accountNumber, [FromBody] TransactionDTO transation)
+        {
+            try
+            {
+                var request = new TransferRequest(accountNumber, transation);
+                var response = request.Transfer();
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Mensagem de erro:" + e.Message);
+            }
+        }
+    }
+}
