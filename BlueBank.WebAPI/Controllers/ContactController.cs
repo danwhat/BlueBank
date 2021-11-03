41cf7ba8c711ad1f957f15b5dcaf26b54e45f1e7
Luciano Berchon <berchon@gmail.com>
2021-11-03 11:29:24 -0300

    delete contatos implementado sem validacoes

---
+++ BlueBank.WebAPI/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Core.DTOs;
+using Domain.Requests;
+using Domain.Services.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlueBank.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        [HttpDelete("{doc}")]
+        public ObjectResult DeleteAccount(string doc, [FromBody] string phoneNumber)
+        {
+            var request = new DeleteContactRequest(doc, phoneNumber);
+
+            try
+            {
+                var result = request.Delete();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Mensagem de erro:" + e.Message);
+            }
+        }
+    }    
+}
---
+++ Domain/Requests/DeleteContactRequest.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Core.DTOs;
+using Domain.Entities;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Requests
+{
+    public class DeleteContactRequest
+    {
+        private readonly string _phoneNumber;
+        private readonly string _doc;
+        private readonly ContactRepository _contactRepository;
+
+        public DeleteContactRequest(string doc, string phoneNumber)
+        {
+            _phoneNumber = phoneNumber;
+            _doc = doc;
+            _contactRepository = new ContactRepository();
+        }
+
+        public Person Delete()
+        {
+            Person result = _contactRepository.RemoveContact(_doc, _phoneNumber);
+            return result;
+        }
+    }
+}
