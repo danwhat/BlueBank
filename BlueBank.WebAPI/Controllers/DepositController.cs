f9f5a6ffcf32a68a6ebd16e294d3851d396b9797
Daniel Oliveira <dan.carvalho94@gmail.com>
2021-11-03 14:52:51 -0300

    Funcionalidade Deposito implementada.

---
+++ BlueBank.WebAPI/Controllers/DepositController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Core.DTOs;
+using Domain.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlueBank.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepositController : ControllerBase
+    {
+        [HttpPost("{accountNumber}")]
+        public ObjectResult Deposit(int accountNumber, [FromBody] TransactionDTO transation)
+        {
+            try
+            {
+                var request = new DepositRequest(accountNumber, transation);
+                var response = request.Deposit();
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Mensagem de erro:" + e.Message);
+            }
+        }
+    }
+}
---
+++ Domain/Requests/DepositRequest.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Core.DTOs;
+using Domain.Entities;
+using Infrastructure.Repositories;
+using System;
+
+namespace Domain.Requests
+{
+    public class DepositRequest
+    {
+        public DepositRequest(int AccountNumber, TransactionDTO dto)
+        {
+            _accountNumber = AccountNumber;
+            _dto = dto;
+            _accountRepository = new AccountRepository();
+            _transactionRepositoy = new TransactionRepository();
+        }
+
+        private readonly int _accountNumber;
+        private readonly TransactionDTO _dto;
+        private readonly AccountRepository _accountRepository;
+        private readonly TransactionRepository _transactionRepositoy;
+
+        public bool Validate()
+        {
+            Account acc = _accountRepository.Get(_accountNumber);
+            // valida se a conta existe
+            if (acc == null) return false;
+            // validado
+            return true;
+        }
+        
+        public TransactionResponseDTO Deposit()
+        {
+            if (!Validate()) throw new Exception("Não passou na validação");
+
+            Account acc = _accountRepository.Get(_accountNumber);
+
+            Transaction transaction = new();
+            transaction.AccountTo = acc;
+            transaction.Value = _dto.Value;
+
+            try
+            {
+                Transaction transactionDB = _transactionRepositoy.Create(transaction);
+                Account accAtualizada = _accountRepository.Get(_accountNumber);
+                var transactionResponse = new TransactionResponseDTO();
+                transactionResponse.Message = "Deposito realizado com sucesso.";
+                transactionResponse.OldBalance = acc.Balance;
+                transactionResponse.CurrentBalance = accAtualizada.Balance;
+                return transactionResponse;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Deu erro aqui.");
+            }
+        }
+    }
+}
