b89d4d6a4f55cb252b06dc10c4b66c43ea7d2e17
Daniel Oliveira <dan.carvalho94@gmail.com>
2021-11-04 10:56:47 -0300

    Conflitos finalmente resolvidos.

---
+++ BlueBank.WebAPI/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Core.DTOs;
+using Domain.Requests;
+using Domain.Services.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlueBank.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        [HttpPost("{accountNumber}")]
+        public ObjectResult CreateContact(int accountNumber, [FromBody] PersonRequestDto phone)
+        {
+            try
+            {
+                var request = new CreateContactRequest(accountNumber, phone);
+                var result = request.Create();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Mensagem de errooooooooo:" + e.Message);
+            }
+        }
+    }    
+}
